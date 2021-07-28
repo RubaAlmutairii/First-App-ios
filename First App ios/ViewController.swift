7e5bc60efa86816269b25bf22f12abe4c10c08d2
Ruba <RubaAlmutairi@gmail.com>
2021-07-28 15:27:52 +0300

    finish all project

--- First App ios/ViewController.swift
+++ First App ios/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    var DATA_KEY = "data_Key"
+    var pastedStrings : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //save data to persistent storage
+        if let loadedStrings = UserDefaults.standard.stringArray(forKey: DATA_KEY) {
+            pastedStrings.append(contentsOf: loadedStrings)
+        }
+        showText()
     }
 
+    func addText(){
+        guard let text =  UIPasteboard.general.string, !pastedStrings.contains(text) else {
+            return
+        }
+        pastedStrings.append(text)
+        //
+        UserDefaults.standard.set(pastedStrings, forKey: DATA_KEY)
+        showText()
 
+    }
+
+    func showText(){
+        textView.text = ""
+        for str in pastedStrings {
+            textView.text.append("\(str)\n\n")
+        }
+    }
+    
+    //clear saved data
+    @IBAction func trashWasPressed(_ sender: Any) {
+        pastedStrings.removeAll()
+        textView.text = ""
+        UserDefaults.standard.removeObject(forKey:  DATA_KEY)
+    }
+    
 }
+
 
